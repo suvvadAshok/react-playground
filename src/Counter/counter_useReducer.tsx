b57f8e9b28e4fb29dbb0f75a569fbd019e35a65d
ashok_suvvada <suvvadashok@gmail.com>
2024-02-09 09:45:17 +0530

    add counter useReducer

---
+++ src/Counter/counter_useReducer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function reducer(state: number, action: { type: string }): number {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return 0;
+  }
+}
+
+export function Counter_useReducer() {
+  const [state, dispatch] = React.useReducer(reducer, 0);
+
+  return (
+    <>
+      <h3>{state}</h3>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </>
+  );
+}
