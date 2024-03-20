924c4b3926adbe54646a060e8c6d57086ec28575
ashokadvait <ashok@advaitlabs.com>
2024-03-20 14:34:22 +0530

    send prop to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import  config  from "./aws-exports";
+import config from "./aws-exports";
+import { signOut } from "aws-amplify/auth";
 
 Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <App signOut={signOut} />
   </React.StrictMode>
 );
