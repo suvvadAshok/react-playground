0b3f41a889e2eb9e5623f23c500b30c6aeecfbc7
ashokadvait <ashok@advaitlabs.com>
2024-03-20 12:30:20 +0530

    add amplify files, backend folder, auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { NumberAnimation } from "./numberAnimation";
+import logo from "./assets/react.svg";
+import {
+  Card,
+  View,
+  Image,
+  Heading,
+  Button,
+  withAuthenticator,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }: { signOut: any }) {
   return (
-    <div className="h-screen w-screen">
-      <NumberAnimation />
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} alt="logo" className="App-logo" />
+        <Heading level={1}>we now have auth</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
